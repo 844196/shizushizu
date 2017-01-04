607c07ee0ff4cf7858926c371792d6a900e9762c
Masaya Takeda <844196@gmail.com>
2017-01-05 05:00:43 +0900

    update

--- app.rb
+++ app.rb
@@ -6,8 +6,10 @@ class App < Sinatra::Base
   set :method do |*methods|
     condition { methods.map {|m| m.to_s.upcase }.include?(request.request_method) }
   end
+  set :show_exceptions, :after_handler
 
   register Sinatra::Namespace
+  register Sinatra::Reloader if settings.development?
 
   get '/' do
     haml :index
@@ -20,33 +22,51 @@ class App < Sinatra::Base
   namespace '/api/v1' do
     helpers do
       def contract(api_path, method, type)
-        JSON.parse(File.read("#{settings.root}/contracts#{api_path.gsub(/(?<=schedules\/).+/, 'schedule')}/#{method}/#{type}.json"))
+        JSON.parse(File.read("#{settings.root}/contracts#{api_path.gsub(/(?<=schedules\/).+/, 'schedule')}/#{method}/#{type.to_s}.json"))
       end
     end
 
     before :method => [:post, :put] do
       begin
         JSON::Validator.validate!(
-          contract(request.path, request.request_method, 'request'),
+          contract(request.path, request.request_method, :request),
           @payload = JSON.parse(request.body.read)
         )
       rescue JSON::ParserError => e
-        content_type :json
-        halt 400, {:status => status(400), :reason => 'Request data was not JSON object' }.to_json
+        env['sinatra.error'] = e
+        halt :status => status(400), :reason => 'Request data was not JSON object'
       rescue JSON::Schema::ValidationError => e
-        content_type :json
-        halt 400, {:status => status(400), :reason => e.message }.to_json
+        env['sinatra.error'] = e
+        halt :status => status(400), :reason => e.message
+      end
+    end
+
+    after do
+      case
+      when response.status === 204 then
+      when response.body === Hash, Array then
+        begin
+          unless env['sinatra.error']
+            JSON::Validator.validate!(
+              contract(request.path, request.request_method, :response),
+              response.body
+            )
+          end
+        rescue => e
+          response.body = {:status => status(500), :reason => (settings.development? ? e.message : 'Internal server error')}
+        ensure
+          content_type :json
+          response.body = response.body.to_json
+        end
       end
     end
 
     error ActiveRecord::RecordNotFound do
-      content_type :json
-      {:status => status(404), :reason => 'Requested data was not found'}.to_json
+      {:status => status(404), :reason => 'Requested data was not found'}
     end
 
     error do
-      content_type :json
-      {:status => status(500), :reason => (settings.development? ? env['sinatra.error'].message : 'Internal server error') }.to_json
+      {:status => status(500), :reason => (settings.development? ? env['sinatra.error'].message : 'Internal server error') }
     end
 
     post '/speech' do
@@ -54,30 +74,24 @@ class App < Sinatra::Base
     end
 
     get '/speech/schedules' do
-      schedules = SpeechSchedule.all
-      content_type :json
-      schedules.to_json
+      SpeechSchedule.all.map{|s| s.attributes }
     end
 
     post '/speech/schedules' do
-      schedule = SpeechSchedule.new(@payload)
-      schedule.save
-      content_type :json
+      schedule = SpeechSchedule.save_by(@payload)
       status 201
       headers['Location'] = "/api/v1/speech/schedules/#{schedule.id}"
-      schedule.to_json
+      schedule.attributes
     end
 
     put '/speech/schedules/:id' do
-      schedule = SpeechSchedule.find(params['id'])
-      schedule.update(@payload)
-      status 204
+      SpeechSchedule.update_by(@payload)
       headers['Location'] = "/api/v1/speech/schedules/#{params['id']}"
+      status 204
     end
 
     delete '/speech/schedules/:id' do
-      schedule = SpeechSchedule.find(params['id'])
-      schedule.destroy
+      SpeechSchedule.find(params['id']).destroy
       status 204
     end
   end
--- app/models/speech_schedule.rb
+++ app/models/speech_schedule.rb
@@ -1,2 +1,21 @@
 class SpeechSchedule < ActiveRecord::Base
+  def self.save_by(hash)
+    entity = self.new do |me|
+      me.name = hash['name']
+      me.frequency = hash['frequency'] if hash.has_key?('frequency')
+      me.at = hash['at']
+      me.speech_text = hash['speech_text']
+    end
+    entity.tap(&:save)
+  end
+
+  def self.update_by(hash)
+    entity = self.find(hash['id']).tap do |me|
+      me.name = hash['name']
+      me.frequency = hash['frequency']
+      me.at = hash['at']
+      me.speech_text = hash['speech_text']
+    end
+    entity.tap(&:save)
+  end
 end
